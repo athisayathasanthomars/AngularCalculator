f27a9e9d6267fbd5584b2dd9bfb2d95bfd1023fa
athisayathasanthomars <thomarsthuvaa@gmail.com>
2025-01-29 23:45:31 +0530

    commit after ts functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CalculatorComponent } from './calculator/calculator.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,CalculatorComponent],
+  imports: [RouterOutlet,CalculatorComponent,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -7,5 +7,75 @@ import { Component } from '@angular/core';
   styleUrl: './calculator.component.css'
 })
 export class CalculatorComponent {
+  currentInput: string = '';
+  previousInput: string = '';
+  operator: string = '';
+  result: string = '0';
+  
+  NumberClick(value: string) {
+    if (value === '.' && this.currentInput.includes('.')) {
+      return; // Prevent multiple decimal points
+    }
 
+    this.currentInput += value;
+    this.result = this.currentInput;
+  }
+
+  OperatorClick(op: string) {
+    if (this.currentInput === '') return;
+
+    if (this.previousInput !== '') {
+      this.operator = op;
+      this.findResult();
+    }
+
+    this.operator = op;
+    this.previousInput = this.currentInput;
+    this.currentInput = '';
+  }
+
+  findResult() {
+    console.log(this.operator);
+    if (this.previousInput === '' || this.currentInput === '' || this.operator === '') return;
+    
+    let num1 = parseFloat(this.previousInput);
+    let num2 = parseFloat(this.currentInput);
+    let calValue = 0;
+
+    switch (this.operator) {
+      case '+':
+        calValue = num1 + num2;
+        break;
+      case '-':
+        calValue = num1 - num2;
+        break;
+      case '*':
+        calValue = num1 * num2;
+        break;
+      case '/':
+        calValue = num2 !== 0 ? num1 / num2 : NaN; // Prevent division by zero
+        break;
+      case '%':
+        calValue = num1 % num2;
+        break;
+    }
+
+    this.result = calValue.toString();
+    this.previousInput = this.result;
+    this.currentInput = '';
+    this.operator = '';
+    console.log(this.previousInput);
+  }
+
+  clearCalculator() {
+    this.currentInput = '';
+    this.previousInput = '';
+    this.operator = '';
+    this.result = '0';
+  }
+
+  Backspace() {
+    this.currentInput = this.currentInput.slice(0, -1);
+    this.result = this.currentInput || '0';
+  }
 }
